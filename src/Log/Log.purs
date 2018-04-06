0e8e54e6ec9654905dce3084b4f574ee5140ed91
Paul Victor Raj <paulvictor@gmail.com>
2018-04-06 12:57:19 +0530

    Bringing it to a decent shape

--- src/Log/Log.purs
+++ src/Log/Log.purs
@@ -1,31 +1,26 @@
 module Log where
 
-  import Prelude
-  import Data.Foreign.Class
+  import Prelude (class Monad, class Show, Unit, bind, discard, pure, unit, ($), (<$>))
+  import Data.Foreign.Class (class Decode, class Encode)
   import Data.Maybe (Maybe(..))
   import Data.Generic.Rep (class Generic)
-  import Control.Monad.Eff
-  import Control.Monad.State.Class
-  import Data.Array
-  import Partial.Unsafe
-  import Data.Either
-  import Control.Monad.Except
-  import Data.Foreign.Generic
-  import Control.Monad.State.Trans
-  import Control.Monad.Eff.Console
-  import Data.Tuple
-  import Control.Monad.Eff.Class
-  import Data.Foreign
-  import Data.Generic
-  import Data.Generic.Rep.Show
-  import Control.Monad.Eff.Exception.Unsafe
-  import Partial
+  import Control.Monad.Eff (Eff, kind Effect)
+  import Data.Array (uncons, (:))
+  import Partial.Unsafe (unsafePartial)
+  import Data.Either (fromRight)
+  import Control.Monad.Except (runExcept)
+  import Data.Foreign.Generic (decodeJSON, defaultOptions, encodeJSON, genericDecode, genericEncode)
+  import Control.Monad.State.Trans (StateT, get, modify, put, runStateT)
+  import Control.Monad.Eff.Console (CONSOLE, log)
+  import Data.Tuple (fst)
+  import Control.Monad.Eff.Class (class MonadEff, liftEff)
+  import Data.Generic.Rep.Show (genericShow)
 
   foreign import data READLOG :: Effect
   foreign import data WRITELOG :: Effect
-  {--foreign import logStr :: ∀eff. Eff (readlog :: READLOG|eff) (Maybe String)--}
-  {--foreign import emptyLog :: ∀eff. Eff (wlog :: WRITELOG|eff) Unit--}
-  {--foreign import writeLog :: ∀eff. String → Eff (wlog :: WRITELOG|eff) Unit--}
+  foreign import logStr :: ∀eff. Eff (readlog :: READLOG|eff) (Maybe String)
+  foreign import emptyLog :: ∀eff. Eff (wlog :: WRITELOG|eff) Unit
+  foreign import writeLog :: ∀eff. String → Eff (wlog :: WRITELOG|eff) Unit
 
   data LogEntry = Executing | Result String
 
@@ -58,6 +53,13 @@ module Log where
   runLog :: ∀m a. (Monad m) ⇒ StateT Log m a → Log → m a
   runLog s init = fst <$> runStateT s init
 
+  {--suspend :: ∀m a. MonadEff m ⇒ StateT Log m a → StateT Log m a--}
+  suspend :: ∀ m eff. MonadEff (wlog :: WRITELOG | eff ) m ⇒ StateT Log m Unit
+  suspend = do
+    (Log p rpl) ← get
+    _ ← liftEff $ writeLog (encodeJSON p)
+    pure unit
+
   logged :: ∀ m a eff. Monad m ⇒ Encode a ⇒ Decode a ⇒ MonadEff ( console :: CONSOLE | eff) m ⇒ StateT Log m a → StateT Log m a
   logged s = do
     (Log l replays) ← get
@@ -66,8 +68,8 @@ module Log where
         modify \(Log a _) → Log (Executing : a) replays
         res ← s
         put $ Log ((Result $ encodeJSON res) : l) replays
-        {--l' ← get--}
-        {--_ ← liftEff $ log (encodeJSON l')--}
+        l' ← get
+        _ ← liftEff $ log (encodeJSON l')
         pure res
       Just {head: x, tail: xs} → case x of
                                   Executing → do -- We have not yet executed the StateT
@@ -82,17 +84,17 @@ module Log where
 
     pure x
 
-  flow :: ∀ m eff . Monad m ⇒ MonadEff ( console :: CONSOLE | eff ) m ⇒ StateT Log m Foreign
-  flow = logged $ logged $ do
-    r ← logged $ pure 5
-    _ ← logged $ liftEff $ (toForeign <$> logShow (Tuple "A" r))
-    _ ← logged $ liftEff $ (toForeign <$> logShow (Tuple "B" r))
-    logged $ liftEff $ (toForeign <$> logShow (Tuple "C" r))
+  {--flow :: ∀ m eff . Monad m ⇒ MonadEff ( console :: CONSOLE | eff ) m ⇒ StateT Log m Foreign--}
+  {--flow = logged $ logged $ do--}
+    {--r ← logged $ pure 5--}
+    {--_ ← logged $ liftEff $ (toForeign <$> logShow (Tuple "A" r))--}
+    {--_ ← logged $ liftEff $ (toForeign <$> logShow (Tuple "B" r))--}
+    {--logged $ liftEff $ (toForeign <$> logShow (Tuple "C" r))--}
 
-  main = void $ runLog flow (Log [] []) >>= (logShow <<< typeOf)
+  {--main = void $ runLog flow (Log [] []) >>= (logShow <<< typeOf)--}
 
-  savedFlow = "{\"contents\":[[{\"contents\":\"{}\",\"tag\":\"Result\"},{\"contents\":\"5\",\"tag\":\"Result\"},{\"tag\":\"Executing\"},{\"tag\":\"Executing\"}],[]],\"tag\":\"Log\"}"
+  {--savedFlow = "{\"contents\":[[{\"contents\":\"{}\",\"tag\":\"Result\"},{\"contents\":\"5\",\"tag\":\"Result\"},{\"tag\":\"Executing\"},{\"tag\":\"Executing\"}],[]],\"tag\":\"Log\"}"--}
 
-  parsedSavedLog = unsafePartial $ fromRight $ ((runExcept $ decodeJSON savedFlow) :: Either _ Log)
+  {--parsedSavedLog = unsafePartial $ fromRight $ ((runExcept $ decodeJSON savedFlow) :: Either _ Log)--}
 
-  recover = void $ runLog flow (Log [] (reverse $ accumulatedLog parsedSavedLog)) >>= (logShow <<< typeOf)
+  {--recover = void $ runLog flow (Log [] (reverse $ accumulatedLog parsedSavedLog)) >>= (logShow <<< typeOf)--}
